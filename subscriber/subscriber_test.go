7fa565f2cbceaf050e01263329ead19cb6e10c34
Joseph Blodgett <Joseph.blodgett@gmail.com>
2017-04-24 17:15:12 -0600

    Router fleshed out

--- subscriber/subscriber_test.go
+++ subscriber/subscriber_test.go
@@ -48,6 +48,8 @@ func TestFilter(t *testing.T) {
 	}
 	go publisher.Listen()
 
+	time.Sleep(1 * time.Second)
+
 	sub, err := NewSubscriber(200)
 	if err != nil {
 		t.Error(err)
@@ -87,6 +89,10 @@ func TestFilter(t *testing.T) {
 }
 
 func TestMultipleSubscriber(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
 	//create publishers
 	pub, err := publisher.NewPublisher("60002", 1000, 10)
 	if err != nil {
@@ -100,6 +106,13 @@ func TestMultipleSubscriber(t *testing.T) {
 	}
 	go pub1.Listen()
 
+	pub2, err := publisher.NewPublisher("60004", 1000, 10)
+	if err != nil {
+		t.Error(err)
+	}
+	go pub2.Listen()
+
+	time.Sleep(1 * time.Second)
 	//create subscriber
 	sub, err := NewSubscriber(200)
 	if err != nil {
@@ -121,24 +134,49 @@ func TestMultipleSubscriber(t *testing.T) {
 	headerb := [24]byte{}
 	copy(headerb[:], "b")
 
+	headerc := [24]byte{}
+	copy(headerc[:], "c")
+
 	time.Sleep(1 * time.Second)
 
 	log.Printf("Writing..")
-	for i := 0; i < 2; i++ {
-		message := fmt.Sprintf("1-%v", i)
-		pub.Write(common.Message{headera, []byte(message)})
-		pub.Write(common.Message{headerb, []byte(message)})
-	}
-	for i := 0; i < 100; i++ {
-		message := fmt.Sprintf("2-%v", i)
-		pub1.Write(common.Message{headera, []byte(message)})
-		pub1.Write(common.Message{headerb, []byte(message)})
-	}
+	go func() {
+		for i := 0; i < 2; i++ {
+			message := fmt.Sprintf("1-%v", i)
+			pub.Write(common.Message{headera, []byte(message)})
+			pub.Write(common.Message{headerb, []byte(message)})
+		}
+	}()
+	go func() {
+		for i := 0; i < 1000; i++ {
+			message := fmt.Sprintf("2-%v", i)
+			pub1.Write(common.Message{headera, []byte(message)})
+			pub1.Write(common.Message{headerb, []byte(message)})
+			if i%100 == 0 {
+				time.Sleep(175 * time.Millisecond)
+			}
+		}
+	}()
 
 	counts := make(map[string]int)
 
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		log.Printf("Starting subscriber 3")
+		sub.Subscribe("localhost:60004", []string{"c"})
+
+		time.Sleep(1 * time.Second)
+		for i := 0; i < 1000; i++ {
+			message := fmt.Sprintf("3-%v", i)
+			pub2.Write(common.Message{headerc, []byte(message)})
+			if i%100 == 0 {
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
 	//read all the messages
-	for i := 0; i < 202; i++ {
+	for i := 0; i < 3002; i++ {
 		message := sub.Read()
 		if message.MessageBody[0] == '1' && message.MessageHeader != headera {
 			t.Error("Filter failed")
@@ -149,11 +187,13 @@ func TestMultipleSubscriber(t *testing.T) {
 			counts["2a"]++
 		} else if message.MessageBody[0] == '2' && message.MessageHeader == headerb {
 			counts["2b"]++
+		} else if message.MessageBody[0] == '3' && message.MessageHeader == headerc {
+			counts["3"]++
 		}
 	}
 
 	assert.Equal(t, counts["1a"], 2)
-	assert.Equal(t, counts["2a"], 100)
-	assert.Equal(t, counts["2b"], 100)
-
+	assert.Equal(t, counts["2a"], 1000)
+	assert.Equal(t, counts["2b"], 1000)
+	assert.Equal(t, counts["3"], 1000)
 }
